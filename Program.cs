56dee73ee1ce2b60cdde7e4124ffc44af11bbbd1
Zharkov <Zharkov@DESKTOP-FC9HHOM>
2025-07-31 16:47:32 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ULernC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(5);
+        }
+
+        static int LeapYear(int year1, int year2)
+        {
+            return year1 - year2;
+        }
+    }
+}
